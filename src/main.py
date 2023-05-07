a7d78257c9ae5da699d1fcbd1df7346d805a96c7
AFKaro <karolayne.2019230005@unicap.br>
2023-05-07 16:52:05 -0300

    update main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
 from src.pipeline.pipeline_builder import PipelineBuilder
+import argparse
+
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--step', default=None, type=str, help='step name')
+    args = vars(parser.parse_args())
+
     pipeline = PipelineBuilder()
     pipeline.prepare_steps()
-    pipeline.run()
+
+    if args["step"]:
+        pipeline.run_by_step_name(args["step"])
+    else:
+        pipeline.run_all()
