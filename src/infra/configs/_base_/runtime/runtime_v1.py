ccaf7a5d2dca879434d336bacb49394c79f9aedb
AFKaro <karolayne.2019230005@unicap.br>
2023-05-10 16:10:35 -0300

    update runtime and lr

--- src/infra/configs/_base_/runtime/runtime_v1.py
+++ src/infra/configs/_base_/runtime/runtime_v1.py
@@ -3,11 +3,14 @@ evaluation = dict(interval=1, metric=['mAP'])
 
 checkpoint_config = dict(interval=5)
 seed = 0
+# yapf:disable
 log_config = dict(
     interval=5,
     hooks=[
         dict(type='TextLoggerHook'),
+        dict(type='TensorboardLoggerHook')
     ])
+# yapf:enable
 custom_hooks = [
     dict(type='NumClassCheckHook'),
 ]
@@ -22,7 +25,7 @@ resume_from = None
 workflow = [('train', 1)]
 
 gpu_ids = range(1)
-device = 'cpu'
+device = 'cuda'
 
 # disable opencv multithreading to avoid system being overloaded
 opencv_num_threads = 0
