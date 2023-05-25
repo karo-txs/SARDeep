d025f9c24472742ba208bc708321f16469c8e757
AFKaro <karolayne.2019230005@unicap.br>
2023-05-25 20:50:21 -0300

    add new yolo cfg

--- src/pipeline/steps/evaluation_step.py
+++ src/pipeline/steps/evaluation_step.py
@@ -46,98 +46,100 @@ class Evaluation(Step):
     def run_step(self):
 
         for result_dir, model_dir in zip(self.results_dir, self.models_dir):
+            try:
+                output_dir = f"""{os.getenv("WORK_DIR")}/evaluate"""
 
-            output_dir = f"""{os.getenv("WORK_DIR")}/evaluate"""
+                with open(f"{result_dir}/eval_voc.json", "r") as jsonFile:
+                    eval_voc = json.load(jsonFile)
 
-            with open(f"{result_dir}/eval_voc.json", "r") as jsonFile:
-                eval_voc = json.load(jsonFile)
+                with open(f"{result_dir}/eval_coco.json", "r") as jsonFile:
+                    eval_coco = json.load(jsonFile)
 
-            with open(f"{result_dir}/eval_coco.json", "r") as jsonFile:
-                eval_coco = json.load(jsonFile)
+                with open(f"{result_dir}/config.json", "r") as jsonFile:
+                    config = json.load(jsonFile)
 
-            with open(f"{result_dir}/config.json", "r") as jsonFile:
-                config = json.load(jsonFile)
+                voc_metrics = dict(AP25=eval_voc["metric"]["iou_thr_0.25"]["AP25"],
+                                   AP50=eval_voc["metric"]["iou_thr_0.5"]["AP50"],
+                                   AP75=eval_voc["metric"]["iou_thr_0.75"]["AP75"],
+                                   params=eval_voc["params"])
 
-            voc_metrics = dict(AP25=eval_voc["metric"]["iou_thr_0.25"]["AP25"],
-                               AP50=eval_voc["metric"]["iou_thr_0.5"]["AP50"],
-                               AP75=eval_voc["metric"]["iou_thr_0.75"]["AP75"],
-                               params=eval_voc["params"])
+                eval_dict = self.merge_dicts(voc_metrics, eval_coco["metric"])
+                eval_dict = self.merge_dicts(eval_dict, eval_voc["timer"])
+                eval_dict = self.merge_dicts(eval_dict, config)
 
-            eval_dict = self.merge_dicts(voc_metrics, eval_coco["metric"])
-            eval_dict = self.merge_dicts(eval_dict, eval_voc["timer"])
-            eval_dict = self.merge_dicts(eval_dict, config)
+                eval_dict["model"] = f"""{self.model["name"]}{self.model["version"]}"""
+                eval_dict["device"] = self.device
+                eval_dict["dataset_train"] = self.model["datasets"]["name"]
+                eval_dict["dataset_train_fold"] = int(self.model["datasets"]["fold"].replace("fold", ""))
+                eval_dict["dataset_test"] = self.model["datasets"]["paths"]["voc"]["test"]["name"]
+                eval_dict["size"] = Path(model_dir).stat().st_size / 1000000
+                eval_dct = {k: [v] for k, v in eval_dict.items()}
 
-            eval_dict["model"] = f"""{self.model["name"]}{self.model["version"]}"""
-            eval_dict["device"] = self.device
-            eval_dict["dataset_train"] = self.model["datasets"]["name"]
-            eval_dict["dataset_train_fold"] = int(self.model["datasets"]["fold"].replace("fold", ""))
-            eval_dict["dataset_test"] = self.model["datasets"]["paths"]["voc"]["test"]["name"]
-            eval_dict["size"] = Path(model_dir).stat().st_size / 1000000
-            eval_dct = {k: [v] for k, v in eval_dict.items()}
+                if not os.path.exists(output_dir):
+                    os.makedirs(output_dir)
 
-            if not os.path.exists(output_dir):
-                os.makedirs(output_dir)
+                df = pd.DataFrame.from_dict(eval_dct)
+                if os.path.isfile(f"{output_dir}/metric_results.csv"):
+                    df.to_csv(f"{output_dir}/metric_results.csv", mode='a', index=False, header=False)
+                else:
+                    df.to_csv(f"{output_dir}/metric_results.csv", mode='a', index=False, header=True)
 
-            df = pd.DataFrame.from_dict(eval_dct)
-            if os.path.isfile(f"{output_dir}/metric_results.csv"):
-                df.to_csv(f"{output_dir}/metric_results.csv", mode='a', index=False, header=False)
-            else:
-                df.to_csv(f"{output_dir}/metric_results.csv", mode='a', index=False, header=True)
+                self.calculate_means()
 
-            self.calculate_means()
+                if "quantization" not in result_dir:
+                    train_epochs = ""
+                    for root, dirs, files in os.walk(f"{self.work_dir}"):
+                        for file in files:
+                            if "log" in file and file.endswith('.log'):
+                                train_epochs = os.path.join(root, file)
 
-            if "quantization" not in result_dir:
-                train_epochs = ""
-                for root, dirs, files in os.walk(f"{self.work_dir}"):
-                    for file in files:
-                        if "log" in file and file.endswith('.log'):
-                            train_epochs = os.path.join(root, file)
+                    file = open(train_epochs, 'r')
+                    lines = file.read().splitlines()
+                    file.close()
 
-                file = open(train_epochs, 'r')
-                lines = file.read().splitlines()
-                file.close()
+                    train_dict = dict(
+                        model=self.model["name"],
+                        dataset_train=self.model["datasets"]["name"],
+                        dataset_train_fold=int(self.model["datasets"]["fold"].replace("fold", "")),
+                        dataset_test=self.model["datasets"]["paths"]["voc"]["test"]["name"]
+                    )
 
-                train_dict = dict(
-                    model=self.model["name"],
-                    dataset_train=self.model["datasets"]["name"],
-                    dataset_train_fold=int(self.model["datasets"]["fold"].replace("fold", "")),
-                    dataset_test=self.model["datasets"]["paths"]["voc"]["test"]["name"]
-                )
+                    for line in lines:
+                        if "mmdet" in line and "Epoch" in line and "val" not in line:
+                            epoch_batch = line.split("\t")[0].split(" ")[-1]
 
-                for line in lines:
-                    if "mmdet" in line and "Epoch" in line and "val" not in line:
-                        epoch_batch = line.split("\t")[0].split(" ")[-1]
+                            try:
+                                result_line = line.split("\t")[1].split(", ")
+                                del result_line[1]
 
-                        try:
-                            result_line = line.split("\t")[1].split(", ")
-                            del result_line[1]
+                                for index, value in enumerate(result_line):
+                                    key_value = value.split(": ")
 
-                            for index, value in enumerate(result_line):
-                                key_value = value.split(": ")
+                                    result_line[index] = f"\"{key_value[0]}\": {key_value[1]}"
 
-                                result_line[index] = f"\"{key_value[0]}\": {key_value[1]}"
+                                result_line = ", ".join(result_line)
 
-                            result_line = ", ".join(result_line)
+                                result_str = ''.join(('{', result_line, '}'))
 
-                            result_str = ''.join(('{', result_line, '}'))
+                                results = ast.literal_eval(result_str)
+                                results["epoch_batch"] = epoch_batch
 
-                            results = ast.literal_eval(result_str)
-                            results["epoch_batch"] = epoch_batch
+                                results_final = dict(epoch_batch=results["epoch_batch"],
+                                                     lr=results["lr"],
+                                                     loss=results["loss"])
 
-                            results_final = dict(epoch_batch=results["epoch_batch"],
-                                                 lr=results["lr"],
-                                                 loss=results["loss"])
+                                final_dict = self.merge_dicts(train_dict, results_final)
+                                final_dict = {k: [v] for k, v in final_dict.items()}
+                                df = pd.DataFrame.from_dict(final_dict)
 
-                            final_dict = self.merge_dicts(train_dict, results_final)
-                            final_dict = {k: [v] for k, v in final_dict.items()}
-                            df = pd.DataFrame.from_dict(final_dict)
-
-                            if os.path.isfile(f"{output_dir}/epoch_results.csv"):
-                                df.to_csv(f"{output_dir}/epoch_results.csv", mode='a', index=False, header=False)
-                            else:
-                                df.to_csv(f"{output_dir}/epoch_results.csv", mode='a', index=False, header=True)
-                        except:
-                            print("Error!")
+                                if os.path.isfile(f"{output_dir}/epoch_results.csv"):
+                                    df.to_csv(f"{output_dir}/epoch_results.csv", mode='a', index=False, header=False)
+                                else:
+                                    df.to_csv(f"{output_dir}/epoch_results.csv", mode='a', index=False, header=True)
+                            except:
+                                print("Error!")
+            except:
+                print("Error!")
 
     def merge_dicts(self, *dict_args):
         result = {}
